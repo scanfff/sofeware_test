6b2e8a17c7bc0aeeb89d98c3aeea60fcba53aaaa
scanfff <1656304636@qq.com>
2024-05-27 12:08:05 +0800

    13

--- notes/lists/tests.py
+++ notes/lists/tests.py
@@ -2,8 +2,27 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+from lists.models import Item
 
 class HomePageTest(TestCase):
+
+    def text_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.onjects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
