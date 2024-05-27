34a776865076d76cb069cec0c672de1095a268de
scanfff <1656304636@qq.com>
2024-05-27 18:12:10 +0800

    14

--- notes/functional_tests.py
+++ notes/functional_tests.py
@@ -11,10 +11,10 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self,row_text):
-        table = self.browser.find_elements(By.ID, 'id_list_table')
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')  # Corrected here
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text,[row.text for row in rows])
+        self.assertIn(row_text, [row.text for row in rows])
 
 
     def test_can_start_a_list_and_retrieve_it_later(self):
